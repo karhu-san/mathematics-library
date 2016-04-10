442855dd98015a79c13cdc874129dfdf821aa19d
karhu-san <john.doe.smiter@gmail.com>
2016-04-10 23:47:03 +0300

    First idea about vector analysis.

---
+++ VectorAnalysis.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using LinearAlgebraLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VectorAnalysis.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestCategory("Defaults"), TestMethod]
+        public void DefaultIntervalIsR2UnitSquareInFirstQuadrant()
+        {
+            var I = new Interval();
+
+            Assert.AreEqual(2, I.Dimension);
+            
+            Assert.AreEqual(0, I.x[0].Low);
+            Assert.AreEqual(1, I.x[0].High);
+
+            Assert.AreEqual(0, I.x[1].Low);
+            Assert.AreEqual(1, I.x[1].High);
+
+        }
+    }
+
+    public class Interval
+    {
+        public List<Interval> x;
+        public int Dimension => x.Count;
+
+        public double Low, High;
+
+        public Interval(double low, double high)
+        {
+            Low = low;
+            High = high;
+        }
+
+        public Interval()
+        {
+            x = new List<Interval>();
+
+            var dim = new Interval(0, 1);
+            x.Add(dim);
+            
+            dim = new Interval(0, 1);
+            x.Add(dim);
+        }
+    }
+}
